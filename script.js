7227d7d0b4f583c75feb2c3bcab588771e949192
kaliberpoziomka <34923929+kaliberpoziomka@users.noreply.github.com>
2020-04-17 18:22:12 +0200

    final version

--- script.js
+++ script.js
@@ -1,1 +1,91 @@
 
+const container = document.getElementById('container');
+const whiteButton = document.getElementById('white');
+const randomButton = document.getElementById('random');
+const resetButton = document.getElementById('reset');
+const rainbowButton = document.getElementById('rainbow');
+const cells = document.querySelectorAll('.inside');
+
+
+//CORE
+function makeCells(quantity, color) {
+    let size = 500 / quantity;
+
+    for(let i = 0; i < quantity; i++) {
+        var br = document.createElement("br");
+        
+        for(let j = 0; j < quantity; j++){
+            let cell = document.createElement('div');
+            cell.style.width = size -2 + 'px';
+            cell.style.height = size -2 + 'px';
+            cell.setAttribute('class', 'inside');
+            cell.setAttribute('id', `${i}.${j}`);
+            container.appendChild(cell);
+            cell.addEventListener('mouseover', function (){
+            cell.style.background = '#'+ color;
+            }, false)
+        }
+        
+        container.appendChild(br);
+    }
+}
+
+
+
+// Button functions
+
+//Palette of colors
+palette = ['22d3a1', '1fc11b', 'ffd913', 'ff9c55', 'ff5555'];
+
+
+//Reset
+resetButton.addEventListener('click', gameReset, false);
+function gameReset() {
+    location.reload();
+}
+
+
+//White
+whiteButton.addEventListener('click', startWhite, false)
+function startWhite(){
+    quantity = prompt('Podaj liczbę pól');
+    color = 'ffffff';
+    makeCells(quantity, color);
+ }
+
+
+//Random
+ randomButton.addEventListener('click', startRandom, false);
+ function startRandom(){
+     quantity = prompt('Podaj liczbę pól');
+     color = palette[Math.floor(Math.random() * palette.length)];
+     makeCells(quantity, color);
+  }
+
+//Rainbow
+rainbowButton.addEventListener('click', startRainbow, false);
+
+  function startRainbow() {
+    let quantity = prompt('Podaj liczbę pól');
+    let size = 500 / quantity;
+
+    for(let i = 0; i < quantity; i++) {
+        var br = document.createElement("br");
+        
+        for(let j = 0; j < quantity; j++){
+            let randomColor = palette[Math.floor(Math.random() * palette.length)];
+            let cell = document.createElement('div');
+            cell.style.width = size -2 + 'px';
+            cell.style.height = size -2 + 'px';
+            cell.setAttribute('class', 'inside');
+            container.setAttribute('class', 'containerRainbow');
+            cell.setAttribute('id', `${i}.${j}`);
+            container.appendChild(cell);
+            cell.addEventListener('mouseover', function (){
+            cell.style.background ='#' + randomColor;
+            }, false)
+        }
+        
+        container.appendChild(br);
+    }
+}
